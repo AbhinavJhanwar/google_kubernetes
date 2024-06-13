dda01eb8aa9b46325658628ff0a942533eb09b18
Abhinav Jhanwar (c) <abhinav.jhanwar@valuelabs.com>
2024-06-13 12:11:16 +0000

    sample product on docker

--- product/frontend/user_interface.py
+++ product/frontend/user_interface.py
@@ -2,8 +2,15 @@ import streamlit as st
 import requests
 
 def get_fruits():
-    url = "http://172.17.0.1:8000/fruits"
-    # url = "http://0.0.0.0:8000/fruits"
+    # if running separate docker containers for backend and frontend then 
+    # get the ip address of backend docker container by using typing 
+    # http://localhost:<port>/docs and use that below
+    # url = "http://172.17.0.1:8000/fruits"
+
+    # if runnig using yaml file then url will be as below. where fastapi is the name of backend docker container- 
+    # http://<backend container name>:8000/fruits
+    url = "http://fastapi:8000/fruits"
+
     response = requests.get(url)
     return eval(response.text)
 
