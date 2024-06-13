01f5b769c07cab39912ac81dac7ac7417ffb0c01
Abhinav Jhanwar (c) <abhinav.jhanwar@valuelabs.com>
2024-06-13 07:08:00 +0000

    adding docker

--- product/frontend/user_interface.py
+++ product/frontend/user_interface.py
@@ -2,12 +2,13 @@ import streamlit as st
 import requests
 
 def get_fruits():
-    url = "172.17.0.1:8000/fruits"
+    url = "http://172.17.0.1:8000/fruits"
+    # url = "http://0.0.0.0:8000/fruits"
     response = requests.get(url)
     return eval(response.text)
 
 st.title("Sample Streamlit APP")
 if st.button("Get Fruits"):
-    st.write(", ".join(get_fruits["fruits"]))
+    st.write(", ".join(get_fruits()["fruits"]))
 else:
     st.write("I want some fruits")
